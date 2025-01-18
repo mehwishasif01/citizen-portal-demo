18f608e241a234b9dccc8ea78b7c655ff73a4873
mehwish.asif <mehwish.asif@turingtechnologies.org>
2025-01-18 22:06:32 +0000

    update api url

--- client/src/utils/apiClient.js
+++ client/src/utils/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://citizen-portal-demo-api.onrender.com//api/",
+  baseURL: "https://citizen-portal-demo-api.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
