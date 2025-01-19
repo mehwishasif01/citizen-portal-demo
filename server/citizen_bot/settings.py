0a7bd5a82ddca016974a9a035b4205c4659d36e7
mehwish.asif <mehwish.asif@turingtechnologies.org>
2025-01-19 18:59:37 +0000

    add allowed origin

--- server/citizen_bot/settings.py
+++ server/citizen_bot/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://citizen-portal-demo.vercel.app"
 ]
 
 
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -58,7 +60,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = "citizen_bot.urls"
